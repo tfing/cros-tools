faf30fc19397f9178787fb778597001505fac33f
Tinghan Shen <tinghan.shen@mediatek.com>
2021-12-27 18:56:30 +0800

    dfd: update dump_dfd
    Signed-off-by: Tinghan Shen <tinghan.shen@mediatek.com>

--- dfd/change-dfd-fw.sh
+++ dfd/change-dfd-fw.sh
@@ -7,23 +7,26 @@ fi
 
 IP=$1
 
-echo '[[[ DUT: $IP ]]]'
+echo "[[[ DUT: $IP ]]]"
 
 echo '>>> add ssh key'
 cd /proj/mtk15399/dfd
 ssh-keygen -f "/proj/mtk15399/.ssh/known_hosts" -R "$IP"
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$IP
 
+echo '>>> dump fw/os version'
+ssh root@$IP "echo '--fw--' && crossystem fwid && echo && echo '--ec--' && ectool version && echo '--os--' && uname -a && cat /etc/lsb-release | grep 'DESCRIPTION' && echo --scp-- && md5sum /lib/firmware/scp.img"
+
 echo '>>> tx dfd file/fw'
 scp dump_dfd root@$IP:/usr/bin/
 scp dump_dfd.conf root@$IP:/etc/init/
-scp image-tomato.dev.bin root@$IP:/tmp
 
 echo '>>> print dfd file/fw path'
 ssh root@$IP "sync && ls /usr/bin/dump_dfd* | grep dump_dfd && echo '--' && ls /etc/init/dump_dfd* | grep dump && echo '--' && ls /tmp/image*"
 
 #echo '>>> flash dfd fw'
+#scp image-tomato.dev.bin root@$IP:/tmp
 #ssh root@$IP "flashrom -p host -w /tmp/image-tomato.dev.bin"
 
-echo '>>> reboot'
-ssh root@$IP reboot
+#echo '>>> reboot'
+#ssh root@$IP reboot
